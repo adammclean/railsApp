71bc391dbc14c9d401f3c5312a3c6daaebd74bb5
Adam McLean <adam.mclean1@gmail.com>
2015-03-21 14:03:55 -0700

    patch routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'help' => 'static_pages#help', as: 'help_path'
-  get 'about' => 'static_pages#about', as: 'about_path'
-  get 'contact' => 'static_pages#contact', as: 'contact_path'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
