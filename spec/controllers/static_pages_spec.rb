c73c2808a1e3ec8b1be97b403ce3b3c84927557f
Adam McLean <adam.mclean1@gmail.com>
2015-03-21 14:18:06 -0700

    fix spec test language

--- spec/controllers/static_pages_spec.rb
+++ spec/controllers/static_pages_spec.rb
@@ -1,44 +1,56 @@
-describe StaticPagesController do
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, :type => :controller do
 
   before :each do
     @base_title = "Micro"
   end
 
-  it "should get home"  do
-    get :home
-    expect(response).to render_template :home
-    expect(response).to have_http_status(:success)
-    assert_select "title", "Home | #{@base_title}"
+  describe "GET home" do
+    it "returns HTTP success"  do
+      get :home
+      expect(response).to render_template :home
+      expect(response).to have_http_status(:success)
+      assert_select "title", "Home | #{@base_title}"
+    end
   end
 
-  it "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+  describe "GET help" do
+    it "returns HTTP success" do
+      get :help
+      assert_response :success
+      assert_select "title", "Help | #{@base_title}"
+    end
   end
 
-  it "should get about" do
-    get :about
-    expect(response).to have_http_status(:success)
-    assert_select "title", "About | #{@base_title}"
+  describe "GET about" do
+    it "returns HTTP success" do
+      get :about
+      expect(response).to have_http_status(:success)
+      assert_select "title", "About | #{@base_title}"
+    end
   end
 
-  it "should get contact" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+  describe "GET contact" do
+    it "returns HTTP success" do
+      get :contact
+      assert_response :success
+      assert_select "title", "Contact | #{@base_title}"
+    end
   end
 
   # integration test
-  it "should test layout links" do
-    get :root_path
-    assert template 'static_pages/home'
-    assert_select 'a[href=?]', root_path, count: 2
-    assert_select 'a[href=?]', help_path
-    assert_select 'a[href=?]', about_path
-    assert_select 'a[href=?]', contact_path
+  describe "layout links should have correct paths"
+    it "asserts page paths are correct" do
+      get :root_path
+      assert template 'static_pages/home'
+      assert_select 'a[href=?]', root_path, count: 2
+      assert_select 'a[href=?]', help_path
+      assert_select 'a[href=?]', about_path
+      assert_select 'a[href=?]', contact_path
+    end
   end
-end
+
 
 
 # $ bundle exec rspec
