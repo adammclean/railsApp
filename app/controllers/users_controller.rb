81711f5acf7a1a7db7fddffd2c40e371562e410a
Adam McLean <adam.mclean1@gmail.com>
2015-03-24 11:18:33 -0700

    add create method for user form and strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(User.first.id)
+    @user = User.find(params[:id])
     debugger
   end
 
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successful save
+    else
+      render 'new'
+    end
+  end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
