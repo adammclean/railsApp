d81d8e69e890a87ffe7736ad57dc98effec406ec
Adam McLean <adam.mclean1@gmail.com>
2015-03-31 15:16:58 -0700

    add user index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
@@ -32,6 +32,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
 
   private
 
@@ -41,6 +44,7 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
