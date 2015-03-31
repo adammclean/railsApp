cfccd082b3f951df360dd93fc2849ea6e40ef6a5
Adam McLean <adam.mclean1@gmail.com>
2015-03-31 15:32:05 -0700

    about to drop database and recreate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
+
+
+#creates an example user with name and email address replicating our previous one, and then makes 99 more.
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
