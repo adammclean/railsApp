99ea6d43745d60567e26de29a1bb9a530bf2fe1f
Adam McLean <adam.mclean1@gmail.com>
2015-04-01 16:18:28 -0700

    add reset digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401165037) do
+ActiveRecord::Schema.define(version: 20150401231756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150401165037) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
