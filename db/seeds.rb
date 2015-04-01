b93f37b876b0c5b2271b685166326f47fbe48954
Adam McLean <adam.mclean1@gmail.com>
2015-04-01 08:58:48 -0700

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar",
+  admin: true)
 
 
 #creates an example user with name and email address replicating our previous one, and then makes 99 more.
