a09600521ca40a02b4b3940a77bb4744da5821dc
Adam McLean <adam.mclean1@gmail.com>
2015-04-03 08:18:09 -0700

    model and seed following and followers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
 
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
@@ -12,7 +9,13 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  resources :users
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
