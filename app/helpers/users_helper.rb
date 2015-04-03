5cfcf7be07a975ecf9dd2277773acee4f9057e2e
Adam McLean <adam.mclean1@gmail.com>
2015-04-03 07:12:34 -0700

    add carrierwave and imagemagick need to fix bugs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,8 @@
 module UsersHelper
 
   # We create this method and call on it in our users index view
+  # @return [Object]
   def gravatar_for(user)
-    # do somethings with the options hash
-    #debug(options)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
